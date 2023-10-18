a8be53d756458a9b10446b17813619dad986e559
manop51 <107394216+manop51@users.noreply.github.com>
2023-10-18 11:29:57 +0300

    Update Book.java

--- Book.java
+++ Book.java
@@ -2,9 +2,8 @@ package studyopedia;
 
 public class Book {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	String title;
+		String author;
+		int numberOfPages
 
 }
