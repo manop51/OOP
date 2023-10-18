5a747af72b85cc8d35f0d3b69845cf0adf63ed9d
manop51 <107394216+manop51@users.noreply.github.com>
2023-10-18 11:29:24 +0300

    Update BookInputTest.java

--- BookInputTest.java
+++ BookInputTest.java
@@ -4,7 +4,22 @@ public class BookInputTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Book myBook =new Book();
+		
+		Scanner item = new Scanner(System.in);
+		
+		System.out.println("please enter The title of the book");
+		 myBook.title= item.nextLine();
+		 
+		 System.out.println("please enter The author of the book");
+		 myBook.author= item.nextLine();
+		 
+		 System.out.println("please enter The number of pages");
+		 myBook.numberOfPages= item.nextInt();
+		 
+		 System.out.println("The book title is:" +myBook.title);
+		 System.out.println("The book author is:"+ myBook.author);
+		 System.out.println("The book title is:"+myBook.numberOfPages);
 	}
 
 }
